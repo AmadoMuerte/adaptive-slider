14fd1567643fd496959bf8c411902260238ac1a9
Amado <kisik854@gmail.com>
2022-02-06 11:09:19 +1000

    create adaptive slider

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const images = document.querySelectorAll('.slider .slider-line img'),
+      sliderLine = document.querySelector('.slider-line');
+
+let btnNext = document.querySelector('.btn-next'),
+    btnPrev = document.querySelector('.btn-prev');
+
+let count = 0;
+let width;
+
+function init() {
+    let slider = document.querySelector('.slider');
+    width = slider.offsetWidth;
+    sliderLine.style.width = width*images.length + 'px';
+    
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+
+    rollSlider();   
+}
+
+function rollSlider() {
+    sliderLine.style.transform = 'translate(-'+count*width +'px)';
+}
+
+window.addEventListener('resize', init);
+
+btnNext.addEventListener('click', () => {
+    count++;
+    if (count >= images.length) {
+        count = 0;
+    }
+
+    rollSlider();
+});
+
+btnPrev.addEventListener('click', () => {
+    count--;
+    if (count < 0) {
+        count = images.length-1;
+    }
+
+    rollSlider();
+});
+
